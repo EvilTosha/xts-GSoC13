2a74b49bb70cc8eed82a79d4b1285bcb52c429e4
Anton Samoylov <toshaevil@gmail.com>
2013-07-04 10:22:41 +0400

    new structure of xtsdfn (tests unchanged)

--- xts/pkg/xtsExtra/R/xtsdfn.R
+++ xts/pkg/xtsExtra/R/xtsdfn.R
@@ -1,36 +1,51 @@
-##' Function for creating xtsdf object (2013 edition prototype)
-##'
-##' .. content for \details{} ..
-##' @title
-##' @param num an \code{xts} object containing numeric part
-##' @param char an \code{xts} object containing character part
-##' @param index logical vector of length \eqn{ncol(num) + ncol(char))} with \eqn{ncol(num)}
-##' TRUE's and \eqn{ncol(char)} FALSE's. TRUE indicates numeric column
-##' @return an \code{xtsdf} object
-##' @author EvilTosha
-as.xtsdfn <- function(num, char, index) {
-  x <- list(numeric = num, character = char, index = index)
+xtsdfn <- function(..., column.classes = NULL, index = NULL){
+  if (is.null(index) && !is.xts(..1))
+    stop("First column needs to be an xts-series if index is not provided")
+
+  if(is.null(index)) index <- index(..1)
+
+  dots <- list(...)
+
+  smodes <- sapply(dots, storage.mode)
+
+  x <- list()
+  x$index <- index
+  x$smodes <- smodes
+
+  recycle.columns <- FALSE
+  if (is.null(column.classes))
+    recycle.columns <- TRUE
+
+  for(smode in unique(smodes)) {
+    x[[smode]] <- do.call(cbind, dots[smode == smodes])
+    if (recycle.columns)
+      column.classes <- c(column.classes, rep(smode, ncol(x[[smode]])))
+  }
+
+  x$column.classes <- column.classes
   class(x) <- "xtsdfn"
+
   x
 }
 
-index.xtsdfn <- function(x) index(x$numeric)
+index.xtsdfn <- function(x) x$index
+
+## doesn't work for some reason
+ncol.xtsdfn <- function(x) print(length(x$column.classes))
 
 ## aux index maps each column index in xtsdfn object to indices in numeric and character parts
 ## aux index is only useful in conjuction with original index
-get.aux.index <- function(index) {
-  index.num <- 0
-  index.char <- 0
-  index.aux <- vector("numeric", length(index))
-  for (i in 1:length(index)) {
-    if (index[i]) {
-      index.num <- index.num + 1
-      index.aux[i] <- index.num
-    }
-    else {
-      index.char <- index.char + 1
-      index.aux[i] <- index.char
-    }
+get.aux.index <- function(x) {
+  class.indexes <- list()
+  for (smode in x$smodes)
+    class.indexes[[smode]] <- 0
+
+  ncol <- length(x$column.classes)
+  index.aux <- vector("numeric", ncol)
+  col.classes <- x$column.classes
+  for (i in 1:ncol) {
+    class.indexes[[col.classes[i]]] <- class.indexes[[col.classes[i]]] + 1
+    index.aux[i] <- class.indexes[[col.classes[i]]]
   }
   index.aux
 }
@@ -40,14 +55,8 @@ get.aux.index <- function(index) {
 as.data.frame.xtsdfn <- function(x, row.names = NULL, optional = FALSE, ...) {
   if (is.null(row.names))
     row.names <- index(x)
-  ## warning: ugly code
-  ## for some reason in R changes in variable made inside closure don't change actual variable outside
-  index.aux <- get.aux.index(x$index)
-  xts.list <- lapply(1:length(x$index),
-                     function(i) {
-                       if (x$index[i]) x$numeric[, index.aux[i]]
-                       else            x$character[, index.aux[i]]
-                     })
+  index.aux <- get.aux.index(x)
+  xts.list <- lapply(1:length(x$column.classes), function(i) x[[x$column.classes[i]]][, index.aux[i]])
   class(xts.list) <- "xtsdf"
   as.data.frame(xts.list)
 }
